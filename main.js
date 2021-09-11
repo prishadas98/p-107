55e7b3f09ad3e920ee05c4c4d494ef7dd1ef3bc1
prishadas98 <79792535+prishadas98@users.noreply.github.com>
2021-09-11 11:16:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function start(){
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/YdT2kYnCP/model.json',modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error,results){
+    console.log("got result")
+
+}
